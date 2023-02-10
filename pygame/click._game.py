c1343febb57d346187a035d635be50cd8cb8705c
PiZaRyan <96354418+PiZaRyan@users.noreply.github.com>
2023-02-10 08:55:49 -0500

    Add files via upload

--- pygame/click._game.py
+++ pygame/click._game.py
@@ -1,5 +1,6 @@
-blob = Actor("person")
+blob = Actor("guy")
 blob.topright = 0, 10
+click = 0
 
 WIDTH = 300
 HEIGHT = blob.height + 20
@@ -7,7 +8,7 @@ HEIGHT = blob.height + 20
 
 def draw():
     screen.clear()
-    screen.fill((128, 0, 0))
+    screen.fill((72, 98, 130))
     blob.draw()
 
 
@@ -15,3 +16,16 @@ def update():
     blob.left = blob.left + 2
     if blob.left > WIDTH:
         blob.right = 0
+
+def on_mouse_down(pos):
+    global click
+    if blob.collidepoint(pos) and click == 0:
+        print("Eek!")
+        blob.image = 'person'
+        click = 1
+    elif blob.collidepoint(pos) and click == 1:
+        print("Eek!")
+        blob.image = 'guy'
+        click = 0
+    else:
+        print("You missed me!")
